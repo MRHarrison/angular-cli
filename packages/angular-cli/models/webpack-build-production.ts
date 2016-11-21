4ef859b0f46ee96021b4223855b9be5531afff31
Matthew Harrison <mrussellharrison@gmail.com>
2016-11-21 14:04:21 -0800

    Turn off gzip

--- packages/angular-cli/models/webpack-build-production.ts
+++ packages/angular-cli/models/webpack-build-production.ts
@@ -33,13 +33,13 @@ export const getWebpackProdConfigPartial = function(projectRoot: string, appConf
         compress: { screw_ie8: true },
         sourceMap: true
       }),
-      new CompressionPlugin({
-          asset: '[path].gz[query]',
-          algorithm: 'gzip',
-          test: /\.js$|\.html$/,
-          threshold: 10240,
-          minRatio: 0.8
-      }),
+      // new CompressionPlugin({
+      //     asset: '[path].gz[query]',
+      //     algorithm: 'gzip',
+      //     test: /\.js$|\.html$/,
+      //     threshold: 10240,
+      //     minRatio: 0.8
+      // }),
       new webpack.LoaderOptionsPlugin({
         options: {
           htmlLoader: {
